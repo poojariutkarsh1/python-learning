f00e6e14bb423734fe4456961afe63ca085a4f7b
Utkarsh Uday Poojari <poojariutkarsh1@gmail.com>
2026-02-23 15:50:25 +0530

    Piku gif yayyaya
    Added comments to clarify photo requirements and resizing.

--- pikugif/pikugif.py
+++ pikugif/pikugif.py
@@ -1,15 +1,17 @@
+# before writing this code, the photos need to be in the same folder
+
 import imageio.v3 as iio
 from PIL import Image
 import numpy as np
 
-filenames = ['piku1.png', 'piku2.png']
+filenames = ['piku1.png', 'piku2.png']  #the names should be exactly of the photos that you have put 
 images = []
 
-size = (500, 500)  # choose whatever size you want
+size = (500, 500)  # this lets us resize the photo here. So done by pillow
 
 for filename in filenames:
     img = Image.open(filename)
     img = img.resize(size)
     images.append(np.array(img))
 
-iio.imwrite('piku.gif', images, duration=500, loop=0)
+iio.imwrite('piku.gif', images, duration=500, loop=0)  #made piku's gif yayayya
